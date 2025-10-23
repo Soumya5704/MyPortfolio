df1dbda2c5477c1381eee7a044dbc5508238f841
Soumya5704 <singhsoumya5704@gmail.com>
2025-10-23 23:52:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Typed.js effect
+const typed = new Typed('#element', {
+  strings: ['Software Developer', 'Full Stack Engineer', 'Problem Solver'],
+  typeSpeed: 70,
+  backSpeed: 40,
+  loop: true
+});
